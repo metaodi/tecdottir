9c19aa86aec7638d3f896621ec305b572ec98938
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2017-03-27 23:44:38 +0200

    Redirect to https

--- app.js
+++ app.js
@@ -11,30 +11,40 @@ var config = {
 };
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
-
-  // install middleware
-  swaggerExpress.register(app);
-
-
-  // add swagger ui
-  var swaggerDocument = swaggerExpress.runner.swagger;
-  var options = {
-      docExpansion: "full",
-      url: "http://" + swaggerDocument.host + "/swagger"
-  };
-  var customCss = '#header { display: none }';
-  app.use('/docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDocument, false, options, customCss));
- 
-  //redirect root to /docs
-  app.get('/', function (req, res) {
-    res.redirect('/docs')
-  })
-
-  var port = process.env.PORT || 10010;
-  app.listen(port);
-
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+    if (err) {
+        throw err;
+    }
+  
+    // install middleware
+    swaggerExpress.register(app);
+  
+  
+    // add swagger ui
+    var swaggerDocument = swaggerExpress.runner.swagger;
+    var options = {
+        docExpansion: "full",
+        url: "http://" + swaggerDocument.host + "/swagger"
+    };
+    var customCss = '#header { display: none }';
+    app.use('/docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDocument, false, options, customCss));
+    
+    //redirect to https
+    app.use(function(req, res, next) {
+        console.log(req.get('Host'));
+        if (!req.secure && req.get('Host') != 'localhost:10010') {
+            return res.redirect(['https://', req.get('Host'), req.url].join(''));
+        }
+        next();
+    });
+   
+    //redirect root to /docs
+    app.get('/', function (req, res) {
+        res.redirect('/docs')
+    })
+  
+  
+    var port = process.env.PORT || 10010;
+    app.listen(port);
+  
+    console.log('try this:\ncurl http://' + swaggerDocument.host + '/measurements');
 });
