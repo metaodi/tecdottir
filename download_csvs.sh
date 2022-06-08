088963dec429a8006fea44001d0e2354a593b2c2
Stefan Oderbolz <oderbolz@gmail.com>
2022-06-08 15:04:56 +0200

    Update download_csvs.sh

--- download_csvs.sh
+++ download_csvs.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# Mythenquai
 wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_mythenquai_2022.csv
+wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_mythenquai_2007-2021.csv
+
+# Tiefenbrunnen
 wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_tiefenbrunnen_2022.csv
+wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_tiefenbrunnen_2007-2021.csv
