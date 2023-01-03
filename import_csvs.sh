9f61b494d2c22e9e221ea366e8d97c050969b41c
Stefan Oderbolz <oderbolz@gmail.com>
2023-01-03 09:27:34 +0100

    Update import_csvs.sh

--- import_csvs.sh
+++ import_csvs.sh
@@ -3,9 +3,11 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Mythenquai
-$DIR/update_database.py -f messwerte_mythenquai_2022.csv -t mythenquai --purge --drop
+$DIR/update_database.py -f messwerte_mythenquai_2023.csv -t mythenquai --purge --drop
+$DIR/update_database.py -f messwerte_mythenquai_2022.csv -t mythenquai
 $DIR/update_database.py -f messwerte_mythenquai_2007-2021.csv -t mythenquai 
 
 # Tiefenbrunnen
-$DIR/update_database.py -f messwerte_tiefenbrunnen_2022.csv -t tiefenbrunnen --purge --drop
+$DIR/update_database.py -f messwerte_tiefenbrunnen_2023.csv -t tiefenbrunnen --purge --drop
+$DIR/update_database.py -f messwerte_tiefenbrunnen_2022.csv -t tiefenbrunnen
 $DIR/update_database.py -f messwerte_tiefenbrunnen_2007-2021.csv -t tiefenbrunnen
