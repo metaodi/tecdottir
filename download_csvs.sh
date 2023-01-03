263fe49102ced3fbf47317ef5ab24c504a70b2c8
Stefan Oderbolz <oderbolz@gmail.com>
2023-01-03 09:59:17 +0100

    Update download_csvs.sh

--- download_csvs.sh
+++ download_csvs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Mythenquai
-wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_mythenquai_2022.csv -O messwerte_mythenquai_2023.csv
+wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_mythenquai_2023.csv -O messwerte_mythenquai_2023.csv
 wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_mythenquai_2022.csv -O messwerte_mythenquai_2022.csv
 wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_mythenquai_2007-2021.csv -O messwerte_mythenquai_2007-2021.csv
 
 # Tiefenbrunnen
-wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_tiefenbrunnen_2022.csv -O messwerte_tiefenbrunnen_2023.csv
+wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_tiefenbrunnen_2023.csv -O messwerte_tiefenbrunnen_2023.csv
 wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_tiefenbrunnen_2022.csv -O messwerte_tiefenbrunnen_2022.csv
 wget https://data.stadt-zuerich.ch/dataset/sid_wapo_wetterstationen/download/messwerte_tiefenbrunnen_2007-2021.csv -O messwerte_tiefenbrunnen_2007-2021.csv
